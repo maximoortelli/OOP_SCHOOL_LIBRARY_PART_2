e710b7ced4a0868c4563ad02e9be3f10afff97ae
Ivonne <ivonnebenites2015@gmail.com>
2023-11-15 13:59:33 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,2 +1,11 @@
 require_relative 'app'
 require_relative 'menu'
+
+app = App.new
+option_processor = Menu.new(app)
+loop do
+  app.display_menu
+  option = gets.chomp.to_i
+  option_processor.process_option(option)
+  puts "\n"
+end
