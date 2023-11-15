76636a3b166ddb11d3eabc972fd73d91b02116f4
maximoortelli <maximoortelli@hotmail.com>
2023-11-15 15:56:10 -0300

    Update main.rb
    Update main.rb

--- main.rb
+++ main.rb
@@ -1,11 +1,2 @@
 require_relative 'app'
 require_relative 'menu'
-
-app = App.new
-option_processor = Menu.new(app)
-loop do
-  app.display_menu
-  option = gets.chomp.to_i
-  option_processor.process_option(option)
-  puts "\n"
-end
